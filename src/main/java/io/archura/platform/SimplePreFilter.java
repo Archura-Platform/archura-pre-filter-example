a4f9b81aedea5c35e5b73636183c7a1c8fc581a7
Can MOGOL <canmogol@gmail.com>
2022-07-22 14:29:51 +0200

    Use UnaryOperator for PreFilters.

--- src/main/java/io/archura/platform/SimplePreFilter.java
+++ src/main/java/io/archura/platform/SimplePreFilter.java
@@ -7,19 +7,20 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.function.ServerRequest;
 
 import java.util.Map;
-import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
 
 @Component
-public class SimplePreFilter implements Consumer<ServerRequest>, Configurable {
+public class SimplePreFilter implements UnaryOperator<ServerRequest>, Configurable {
 
     private Map<String, Object> configuration;
 
     @Override
-    public void accept(ServerRequest request) {
+    public ServerRequest apply(ServerRequest request) {
         final Context context = (Context) request.attributes().get(Context.class.getSimpleName());
         final Logger logger = context.getLogger();
 
         logger.info("request = " + request + " configuration = " + configuration);
+        return request;
     }
 
     @Override
