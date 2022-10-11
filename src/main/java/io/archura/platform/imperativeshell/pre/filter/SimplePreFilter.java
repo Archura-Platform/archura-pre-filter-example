6e6163bfbf0149ebafb579c55a91e858db017591
Can MOGOL <canmogol@gmail.com>
2022-10-11 22:09:22 +0200

    Remove spring dependency.

--- src/main/java/io/archura/platform/imperativeshell/pre/filter/SimplePreFilter.java
+++ src/main/java/io/archura/platform/imperativeshell/pre/filter/SimplePreFilter.java
@@ -1,22 +1,20 @@
 package io.archura.platform.imperativeshell.pre.filter;
 
 import io.archura.platform.api.context.Context;
+import io.archura.platform.api.http.HttpServerRequest;
 import io.archura.platform.api.logger.Logger;
 import io.archura.platform.api.type.Configurable;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.function.ServerRequest;
 
 import java.util.Map;
 import java.util.function.UnaryOperator;
 
-@Component
-public class SimplePreFilter implements UnaryOperator<ServerRequest>, Configurable {
+public class SimplePreFilter implements UnaryOperator<HttpServerRequest>, Configurable {
 
     private Map<String, Object> configuration;
 
     @Override
-    public ServerRequest apply(ServerRequest request) {
-        final Context context = (Context) request.attributes().get(Context.class.getSimpleName());
+    public HttpServerRequest apply(HttpServerRequest request) {
+        final Context context = (Context) request.getAttributes().get(Context.class.getSimpleName());
         final Logger logger = context.getLogger();
 
         logger.info("request = " + request + " configuration = " + configuration);
